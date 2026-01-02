37769f8740ffba8f153f63800153a528b57fa232
Mauricio Salgado <mauricio.salgado@ctw.bmwgroup.com>
2026-01-02 22:09:52 +0000

    UPDATE: added comment for sed

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ REPO_URL=$(git remote get-url origin)
 # MANIFEST CUSTOMIZATION
 
 find app-of-apps -name "*.yaml" -type f | while read -r file; do
+  # Does not work in Linux
   sed -i '' "s|\${REPO_URL}|$REPO_URL|g" "$file"
 done
 

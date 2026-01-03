5b7629ca55a5550d930851ff7690e3e6f1946d3b
Mauricio Salgado <mauricio.salgado@ctw.bmwgroup.com>
2026-01-03 01:39:51 +0000

    UPDATE: sed is now OS agnostic

--- setup.sh
+++ setup.sh
@@ -7,8 +7,9 @@ REPO_URL=$(git remote get-url origin)
 # MANIFEST CUSTOMIZATION
 
 find app-of-apps -name "*.yaml" -type f | while read -r file; do
-  # Does not work in Linux
-  sed -i '' "s|\${REPO_URL}|$REPO_URL|g" "$file"
+  tmp=$(mktemp)
+  sed "s|\${REPO_URL}|$REPO_URL|g" "$file" >"$tmp"
+  mv "$tmp" "$file"
 done
 
 git add app-of-apps

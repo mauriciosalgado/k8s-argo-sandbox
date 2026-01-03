f3085399fcfca02e30fdaec183e2c2f4aba3ee2a
Mauricio Salgado <mauricio.salgado@ctw.bmwgroup.com>
2026-01-03 01:39:57 +0000

    UPDATE: minor improvements

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CONTEXTS=$(cat "contexts.txt")
+CONTEXTS=$(<"contexts.txt")
 REPO_URL=$(git remote get-url origin)
 GIT_HOST=$(echo "$REPO_URL" | sed -E 's/.*@([^:]+):.*/\1/')
 BOOTSTRAP_TEMPLATE="app-of-apps/argocd/bootstrap/root-app-template.yaml"
@@ -19,26 +19,26 @@ fi
 # BOOTSTRAPPING
 
 for ctx in $CONTEXTS; do
-  minikube --profile $ctx start
+  minikube --profile "$ctx" start
 done
 
 for ctx in $CONTEXTS; do
-  kubectl config use-context $ctx
+  kubectl config use-context "$ctx"
 
   echo "Creating namespace"
-  kubectl --context $ctx create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
+  kubectl --context "$ctx" create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
 
   echo "Installing ArgoCD"
-  kubectl --context $ctx apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+  kubectl --context "$ctx" apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-  kubectl --context $ctx -n argocd create configmap argocd-ssh-known-hosts-cm \
+  kubectl --context "$ctx" -n argocd create configmap argocd-ssh-known-hosts-cm \
     --from-literal=ssh_known_hosts="$SSH_KNOWN_HOSTS" \
     --dry-run=client -o yaml | kubectl apply -f -
 
-  kubectl --context $ctx -n argocd rollout restart deployment argocd-repo-server
+  kubectl --context "$ctx" -n argocd rollout restart deployment argocd-repo-server
 
   echo "Creating Repo Credentials Secret"
-  kubectl --context $ctx create secret generic private-repo \
+  kubectl --context "$ctx" create secret generic private-repo \
     -n argocd \
     --from-literal=type=git \
     --from-literal=url="$REPO_URL" \
@@ -50,12 +50,12 @@ for ctx in $CONTEXTS; do
     kubectl apply -f -
 
   echo "Waiting for Argo CD to be ready"
-  kubectl --context $ctx wait deployment argocd-server \
+  kubectl --context "$ctx" wait deployment argocd-server \
     -n argocd \
     --for=condition=Available=True \
     --timeout=300s
 
   echo "Bootstrapping ArgoCD Root Application in $ctx"
-  CLUSTER_ENV=$ctx
+  CLUSTER_ENV="$ctx"
   envsubst <$BOOTSTRAP_TEMPLATE | kubectl apply -f -
 done

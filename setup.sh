b4b4b1939cb42cdd8e5a932e2b7491043d587a1c
Mauricio Salgado <mauricio.salgado@ctw.bmwgroup.com>
2026-01-03 18:08:00 +0000

    UPDATE: idempotent script now

--- setup.sh
+++ setup.sh
@@ -4,7 +4,14 @@ set -euo pipefail
 
 REPO_URL=$(git remote get-url origin)
 
+# CHECK IF ALREADY RUN
+if ! grep -R "\${REPO_URL}" app-of-apps >/dev/null; then
+  echo "Repository already personalized"
+  exit 0
+fi
+
 # MANIFEST CUSTOMIZATION
+echo "Personalizing repository with REPO_URL=$REPO_URL"
 
 find app-of-apps -name "*.yaml" -type f | while read -r file; do
   tmp=$(mktemp)
